069311d3364f4c19efed14081096b2f142aa62b4
Kojo Patrick <patrickasare84@gmail.com>
2021-06-19 11:07:03 +0000

    update info

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,40 +1,47 @@
+const sections = document.querySelectorAll('section'),
+	scrollTop = document.querySelector('.scroll-top'),
+	popup = document.getElementById('popup')
+function showPopup() {
+	setTimeout(() => {
+		popup.classList.add('show')
+	}, 100)
+}
+function removePopup() {
+	setTimeout(() => {
+		popup.classList.remove('show')
+	}, 6e3)
+}
 ;(window.sr = ScrollReveal()),
-	sr.reveal('h1', {
-		delay: 0,
-		duration: 200,
-		origin: 'bottom',
-		distance: '100px',
-	}),
 	$('a[href*="#"]')
 		.not('[href="#"]')
 		.not('[href="#0"]')
-		.click(function (e) {
-			var o
+		.click(function (o) {
+			var e
 			location.pathname.replace(/^\//, '') !=
 				this.pathname.replace(/^\//, '') ||
 				location.hostname != this.hostname ||
-				((o = (o = $(this.hash)).length
-					? o
+				((e = (e = $(this.hash)).length
+					? e
 					: $('[name=' + this.hash.slice(1) + ']')).length &&
-					(e.preventDefault(),
+					(o.preventDefault(),
 					$('html, body').animate(
-						{ scrollTop: o.offset().top },
+						{ scrollTop: e.offset().top },
 						100,
 						function () {
-							var e = $(o)
-							if ((e.focus(), e.is(':focus'))) return !1
-							e.attr('tabindex', '-1'), e.focus()
+							var o = $(e)
+							if ((o.focus(), o.is(':focus'))) return !1
+							o.attr('tabindex', '-1'), o.focus()
 						}
 					)))
+		}),
+	window.addEventListener('scroll', function () {
+		const e = (window.innerHeight / 5) * 4
+		sections.forEach(o => {
+			o.getBoundingClientRect().top < e
+				? (o.classList.add('fadeIn'), scrollTop.classList.add('show'))
+				: (o.classList.remove('fadeIn'), scrollTop.classList.remove('show'))
 		})
-        
-const sections = document.querySelectorAll('section'),
-	scrollTop = document.querySelector('.scroll-top')
-window.addEventListener('scroll', function () {
-	const o = (window.innerHeight / 5) * 4
-	sections.forEach(e => {
-		e.getBoundingClientRect().top < o
-			? (e.classList.add('fadeIn'), scrollTop.classList.add('show'))
-			: (e.classList.remove('fadeIn'), scrollTop.classList.remove('show'))
+	}),
+	window.addEventListener('DOMContentLoaded', function () {
+		showPopup(), removePopup()
 	})
-})
